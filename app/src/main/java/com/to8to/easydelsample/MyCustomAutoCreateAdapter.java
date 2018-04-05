bf5afa5ce870d3ce9adb0cdf560ee8305a98bced
sam474850601 <sam474850601@gmail.com>
2018-04-05 12:00:52 +0800

    add simple

--- app/src/main/java/com/to8to/easydelsample/MyCustomAutoCreateAdapter.java
+++ app/src/main/java/com/to8to/easydelsample/MyCustomAutoCreateAdapter.java
@@ -1,10 +1,29 @@
 package com.to8to.easydelsample;
 
+import com.to8to.easydel.IAutoCreateAdapter;
+import com.to8to.easydel_annotation.ItemData;
+
+import java.util.List;
+
 /**
  * Created by same.li on 2018/4/3.
  */
 
-public    class MyCustomAutoCreateAdapter extends CustomAutoCreateAdapter {
+public    class MyCustomAutoCreateAdapter implements IAutoCreateAdapter {
 
 
+    @Override
+    public ItemData getItemData(int position) {
+        return null;
+    }
+
+    @Override
+    public List<ItemData> getItemListData() {
+        return null;
+    }
+
+    @Override
+    public void setItemListData(List<ItemData> itemListData) {
+
+    }
 }
